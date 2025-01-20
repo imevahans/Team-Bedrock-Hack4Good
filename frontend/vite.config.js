f5c7d0e3813dc39b902861b0d4c636613db12a40
imevahans <ImEvaHans@gmail.com>
2025-01-20 08:48:47 +0800

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,9 +3,6 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist', // Ensure output is placed in the `dist` folder for Vercel
-  },
   base: './',
   server: {
     port: 5173, // Frontend port for local development
